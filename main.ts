453d1dff05d3c2ac1ea7354a1567724d699d8c55
huangshixuan1 <98266496+huangshixuan1@users.noreply.github.com>
2023-07-31 14:50:29 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,104 @@
-basic.forever(function () {
-	
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 4) {
+    	
+    } else if (receivedNumber == 8) {
+    	
+    } else if (receivedNumber == 6) {
+    	
+    } else if (receivedNumber == 2) {
+    	
+    } else if (receivedNumber == 1) {
+    	
+    } else if (receivedNumber == 3) {
+    	
+    } else if (receivedNumber == 7) {
+    	
+    } else if (receivedNumber == 9) {
+    	
+    } else if (receivedNumber == 0) {
+    	
+    }
 })
+function 左轉 () {
+    sensors.DDMmotor(
+    AnalogPin.P0,
+    0,
+    AnalogPin.P0,
+    0
+    )
+    sensors.DDMmotor(
+    AnalogPin.P0,
+    0,
+    AnalogPin.P0,
+    0
+    )
+}
+function 右轉 () {
+    sensors.DDMmotor(
+    AnalogPin.P0,
+    0,
+    AnalogPin.P0,
+    0
+    )
+    sensors.DDMmotor(
+    AnalogPin.P0,
+    0,
+    AnalogPin.P0,
+    0
+    )
+}
+function 把球夾起來 () {
+	
+}
+function 後退 () {
+    sensors.DDMmotor(
+    AnalogPin.P0,
+    0,
+    AnalogPin.P0,
+    0
+    )
+    sensors.DDMmotor(
+    AnalogPin.P0,
+    0,
+    AnalogPin.P0,
+    0
+    )
+}
+function 前進 () {
+    sensors.DDMmotor(
+    AnalogPin.P0,
+    0,
+    AnalogPin.P0,
+    0
+    )
+    sensors.DDMmotor(
+    AnalogPin.P0,
+    0,
+    AnalogPin.P0,
+    0
+    )
+}
+function 把球下降 () {
+	
+}
+function 把球上升 () {
+	
+}
+function 把球放下來 () {
+	
+}
+function 停止 () {
+    sensors.DDMmotor(
+    AnalogPin.P0,
+    0,
+    AnalogPin.P0,
+    0
+    )
+    sensors.DDMmotor(
+    AnalogPin.P0,
+    0,
+    AnalogPin.P0,
+    0
+    )
+}
+radio.setGroup(98)
